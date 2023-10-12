0e9dd887c09b17c9f50f84e4d1ec903f59ef2698
Ananthu Jayakumar <115489088+ananthu666@users.noreply.github.com>
2023-10-12 13:20:50 +0530

    Update dt.py

--- users/dt.py
+++ users/dt.py
@@ -1,12 +1,7 @@
 from datetime import datetime
 
 def get_current_time_formatted():
-    """
-    Get the current time in the format "%Y-%m-%d %H:%M:%S".
-
-    Returns:
-        str: The formatted current time.
-    """
+  
     current_time = datetime.utcnow()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
     return formatted_time
